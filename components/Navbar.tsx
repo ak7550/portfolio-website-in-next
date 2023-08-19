7b19a1e8614b30d6725076ed8a12647f7a82387e
Aniket Kumar Ghosh <ghoshaniketkumar7@gmail.com>
2023-08-19 21:54:04 +0530

    fixed few things

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import Link from 'next/link';
 import React, { useRef, useState } from 'react';
 import { MdOutlineClose } from 'react-icons/md';
 import Footer from './Footer';
-import { userEmail } from '@/shared/UserData';
+import { resumeDriveLink, userEmail } from '@/shared/UserData';
+import { GiCondorEmblem } from 'react-icons/gi'
 
 type Position = {
     x?: number,
@@ -19,9 +19,6 @@ type NavBarItemComponentProps = {
 
 function Navbar () {
     const ref = useRef<HTMLDivElement | null>( null );
-
-    const resumeDriveLink =
-        'https://drive.google.com/file/d/1v6N7c1hof5tUDkbO7-XjreOZqBX6W6TK/view?usp=sharing';
 
     // Use the callback version of useRef to set the ref value
     const setRef = (node: HTMLDivElement) => ref.current = node;
@@ -97,70 +94,84 @@ function Navbar () {
     const closeSideBar = ( e: React.MouseEvent ) => setshowMenu( false )
 
   return (
-      <div className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4'>
-          <div className='max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between'>
-              <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 2 }}
-              >
-                  <Image src={``} alt="logo" className="w-14" />
-              </motion.div>
-              <div className="hidden mdl:inline-flex items-center gap-7">
-                  <ul className='flex text-[13px] gap-7'>
-                      <NavBarItemComponent initialPosition={ { y: -10, opacity: 0 } } finalPosition={ { y: 0, opacity: 1 } } />
-                  </ul>
-              <ResumeComponent />
-              </div>
-              {/* small icon section, try to keep the animation while hovering over the icon */}
-              <div onClick={() => setshowMenu(true)}
-                  className='w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl
-                  text-textGreen cursor-pointer overflow-hidden group'>
-                  <span
-                      className='w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all
-                      ease-in-out duration-300'
-                  ></span>
-                  <span
-                      className='w-full h-[2px] bg-textGreen inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all
-                      ease-in-out duration-300'
-                  ></span>
-                  <span
-                      className='w-full h-[2px] bg-textGreen inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all
-                      ease-in-out duration-300'
-                  ></span>
-              </div>
-              {
-                  showMenu &&
-                  <div ref={ ( node: HTMLDivElement ) => ref.current = node } onClick={closeSideBar}
-                    className='absolute mdl:hidden top-0 right-0 w-full h-screen bg-black bg-opacity-50
-                     flex flex-col items-end'>
-                      <motion.div
-                          initial={ { x: 20, opacity: 0 } }
-                          animate={ { x: 0, opacity: 1 } }
-                          transition={ { duration: 0.3 } }
-                          className='w-[80%] h-full bg-[#112240] flex flex-col items-center px-4 py-10 relative'
-                      >
-                          <MdOutlineClose className='text-3xl text-textGreen cursor-pointer hover:text-red-500 absolute top-4 right-4' />
-                          <div className='flex flex-col items-center gap-7'>
-                              <ul className='flex flex-col text-base gap-7'>
-                                  <NavBarItemComponent initialPosition={{x: 10, opacity:0}} finalPosition={{x:0, opacity:1}} />
-                              </ul>
-                            <ResumeComponent />
-                          <Footer />
-                              </div>
-                              <motion.a initial={ { x: 10, opacity: 0 } }
-                                  animate={ { x: 0, opacity: 1 } }
-                                  transition={ { delay: 0.8, ease: 'easeIn' } }
-                                  href={ `mailto:${ userEmail }` }
-                                  className='text-sm w-72 tracking-widest text-textGreen text-center mt-4'
-                              >
-                                  <p>{ userEmail }</p>
-                              </motion.a>
-                          </motion.div>
-                  </div>
-              }
+    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
+      <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          <GiCondorEmblem
+            className="w-10 h-10 ml-[30%]
+                        hover:text-textGreen cursor-pointer hover:transition-all duration-300"
+          />
+        </motion.div>
+        <div className="hidden mdl:inline-flex items-center gap-7">
+          <ul className="flex text-[13px] gap-7">
+            <NavBarItemComponent
+              initialPosition={{ y: -10, opacity: 0 }}
+              finalPosition={{ y: 0, opacity: 1 }}
+            />
+          </ul>
+          <ResumeComponent />
         </div>
+        {/* small icon section, try to keep the animation while hovering over the icon */}
+        <div
+          onClick={() => setshowMenu(true)}
+          className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl
+                  text-textGreen cursor-pointer overflow-hidden group"
+        >
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all
+                      ease-in-out duration-300"
+          ></span>
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all
+                      ease-in-out duration-300"
+          ></span>
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all
+                      ease-in-out duration-300"
+          ></span>
+        </div>
+        {showMenu && (
+          <div
+            ref={(node: HTMLDivElement) => (ref.current = node)}
+            onClick={closeSideBar}
+            className="absolute mdl:hidden top-0 right-0 w-full h-screen bg-black bg-opacity-50
+                     flex flex-col items-end"
+          >
+            <motion.div
+              initial={{ x: 20, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              className="w-[80%] h-full bg-[#112240] flex flex-col items-center px-4 py-10 relative"
+            >
+              <MdOutlineClose className="text-3xl text-textGreen cursor-pointer hover:text-red-500 absolute top-4 right-4" />
+              <div className="flex flex-col items-center gap-7">
+                <ul className="flex flex-col text-base gap-7">
+                  <NavBarItemComponent
+                    initialPosition={{ x: 10, opacity: 0 }}
+                    finalPosition={{ x: 0, opacity: 1 }}
+                  />
+                </ul>
+                <ResumeComponent />
+                <Footer />
+              </div>
+              <motion.a
+                initial={{ x: 10, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: 0.8, ease: 'easeIn' }}
+                href={`mailto:${userEmail}`}
+                className="text-sm w-72 tracking-widest text-textGreen text-center mt-4"
+              >
+                <p>{userEmail}</p>
+              </motion.a>
+            </motion.div>
+          </div>
+        )}
       </div>
+    </div>
   )
 }
 
--- components/middleComponents/Banner.tsx
+++ components/middleComponents/Banner.tsx
@@ -2,61 +2,61 @@ import { userBio, userFirstName } from '@/shared/UserData'
 import { motion } from 'framer-motion'
 import React from 'react'
 
-
 const Banner = () => {
   return (
-      <section
-          id='home'
-          className='max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4
-          lgl:gap-8 mdl:px-10 xl:px-4'
+    <section
+      id="home"
+      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4
+          lgl:gap-8 mdl:px-10 xl:px-4"
+    >
+      <motion.h3
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6, duration: 0.5 }}
+        className="text-lg font-titleFont tracking-wide text-textGreen"
       >
-          <motion.h3
-                initial={{opacity: 0, y:10}}
-                animate={{opacity:1, y:0}}
-                transition={{delay: 0.6, duration: 0.5}}
-              className='text-lg font-titleFont tracking-wide text-textGreen'
-          >
-              Hi, My Name is
-          </motion.h3>
+        Hi, My Name is
+      </motion.h3>
 
-          <motion.h1
-                initial={{opacity: 0, y:10}}
-                animate={{opacity:1, y:0}}
-              transition={{ delay: 0.7, duration: 0.5 }}
-              className='text-4xl lgl:text-6xl font-titleFont font-semibold flex flex-col'
-          >
-              {userFirstName}.
-              <span
-                  className='text-textDark mt-2 lgl:mt-4'
-              >
-                  I am a noob web Developer</span>
-          </motion.h1>
+      <motion.h1
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className="text-4xl lgl:text-6xl font-titleFont font-semibold flex flex-col"
+      >
+        {userFirstName}.
+        <span className="text-textDark mt-2 lgl:mt-4">
+          I am a noob web Developer
+        </span>
+      </motion.h1>
 
-          <motion.p
-              initial={{opacity: 0, y:10}}
-                animate={{opacity:1, y:0}}
-                transition={{delay: 0.8, duration: 0.5}}
-              className='text-base md:max-w-[650px] text-textDark font-medium'
-          >
-              {userBio}
-              <a href="http://" target='_blank' rel="noopener noreferrer">
-                <span
-                  className='text-textGreen inline-flex relative cursor-pointer h-7 overflow-x-hidden group'
-                >
-                      Learn More.
-                      <span
-                          className='absolute w-full h-[1px] bg-textGreen left-0 bottom-1 translate-x-[110%]
-                          group-hover:translate-x-0 transition-transform duration-500'
-                      ></span>
-                </span>
-              </a>
-          </motion.p>
+      <motion.p
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.8, duration: 0.5 }}
+        className="text-base md:max-w-[650px] text-textDark font-medium"
+      >
+        {userBio}
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <span className="text-textGreen inline-flex relative cursor-pointer h-7 overflow-x-hidden group">
+            Learn More.
+            <span
+              className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1 translate-x-[110%]
+                          group-hover:translate-x-0 transition-transform duration-500"
+            ></span>
+          </span>
+        </a>
+      </motion.p>
 
-          <motion.button
-              className='w-52 h-14 text-sm font-titleFont border border-textGreen rounded tracking-wide hover:bg-hoverColor duration-300'
-          >
-              Check out my Projects!
-          </motion.button>
+      <motion.button
+        className="w-52 h-14 text-sm font-titleFont border border-textGreen rounded tracking-wide hover:bg-hoverColor duration-300"
+        onClick={(e) => {
+          e.preventDefault()
+          location.href = '#project'
+        }}
+      >
+        Check out my Projects!
+      </motion.button>
     </section>
   )
 }
